4240ceb9e54f875b055370bd3fe9d211cf878e98
danmerus <leoghost@mail.ru>
2018-10-29 15:32:11 +0500

    v1

--- src/main/scala/fintech/homework06/Eq.scala
+++ src/main/scala/fintech/homework06/Eq.scala
@@ -6,5 +6,29 @@ package fintech.homework06
 */
 
 trait Eq[A] {
-  def equiv(lft: A, rgt: A)
+  def equiv(lft: A, rgt: A): Boolean
+}
+
+object Eq {
+    implicit def intEq[Int]: Eq[Int] = new Eq[Int] {
+      def equiv(lft: Int, rgt: Int):Boolean = lft == rgt
+    }
+    implicit def mapEq[A: Eq]: Eq[Map[A,A]] = new Eq[Map[A,A]]{
+      def equiv(lft: Map[A,A], rgt: Map[A,A]): Boolean = {
+        val eqA = implicitly[Eq[A]]
+        lft == rgt
+      }
+    }
+    implicit def seqEq[A: Eq]: Eq[Seq[A]] = new Eq[Seq[A]]{
+      def equiv(lft: Seq[A], rgt: Seq[A]): Boolean = {
+        val eqA = implicitly[Eq[A]]
+        lft == rgt
+        }
+      }
+    implicit def optionEq[A: Eq]: Eq[Option[A]] = new Eq[Option[A]]{
+      def equiv(lft: Option[A], rgt: Option[A]): Boolean = {
+        val eqA = implicitly[Eq[A]]
+        lft == rgt
+      }
+    }
 }
--- src/test/scala/fintech/homework04/EqSpec.scala
+++ src/test/scala/fintech/homework04/EqSpec.scala
@@ -2,5 +2,39 @@ package fintech.homework04
 import org.scalatest.{FlatSpec, Matchers}
 
 class EqSpec extends FlatSpec with Matchers {
-  ???
+  it should "compare maps correctly 1" in {
+    val m1 = Map("a" -> 2)
+    val m2 = Map("a" -> 2)
+    m1 === m2 shouldEqual true
+  }
+  it should "compare maps correctly 2" in {
+    val m1 = Map("a"->2)
+    val m2 = Map("a"->2, "n"->2)
+    m1 === m2 shouldEqual false
+  }
+  it should "compare maps correctly 3" in {
+    val m1 = Map("a"->2)
+    val m2 = Map("a"->3)
+    m1 === m2 shouldEqual false
+  }
+  it should "compare seqs correctly 1" in {
+    val l1 = List(1,2,3)
+    val l2 = List(1,2,3)
+    l1 === l2 shouldEqual true
+  }
+  it should "compare seqs correctly 2" in {
+    val l1 = List(1,2,4)
+    val l2 = List(1,2,3)
+    l1 === l2 shouldEqual false
+  }
+  it should "compare option correctly 1" in {
+    val o1 = Option(2)
+    val o2 = Option(2)
+    o1 === o2 shouldEqual true
+  }
+  it should "compare option correctly 2" in {
+    val o1 = Option(1)
+    val o2 = Option(2)
+    o1 === o2 shouldEqual false
+  }
 }
