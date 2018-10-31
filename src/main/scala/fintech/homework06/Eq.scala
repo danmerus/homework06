bf96af783bd16156ae0285325709cbbefd2a9972
danmerus <leoghost@mail.ru>
2018-10-31 15:35:16 +0500

    v2

--- src/main/scala/fintech/homework06/Eq.scala
+++ src/main/scala/fintech/homework06/Eq.scala
@@ -1,28 +1,69 @@
 package fintech.homework06
+import scala.math._
 
 /*
 Реализовать тайп класс Eq[A] и синтаксис '===', деривацию для Map Seq Option
 Опционально - разработать === для комплексных чисел с возможностью указать точность
 */
 
+case class Complex(re: Double, im: Double, epsilon: Double) extends Ordered[Complex] {
+  private val module = sqrt(pow(re, 2) + pow(im, 2))
+
+  def this(re: Double) = this(re, 0, 0)
+
+  def +(c: Complex) = Complex(re + c.re, im + c.im, epsilon)
+  def -(c: Complex): Complex = this + c*(-1)
+  def *(c: Int) =
+    Complex(re * c, im * c, epsilon)
+  def *(c: Complex) =
+    Complex(re * c.re - im * c.im, im * c.re + re * c.im, epsilon)
+
+  override def compare(that: Complex): Int = this.module compare that.module
+}
 trait Eq[A] {
   def equiv(lft: A, rgt: A): Boolean
 }
 
 object Eq {
-    implicit def intEq[Int]: Eq[Int] = new Eq[Int] {
+  implicit def intEq[Int]: Eq[Int] = new Eq[Int] {
       def equiv(lft: Int, rgt: Int):Boolean = lft == rgt
     }
+  implicit def strEq[String]: Eq[String] = new Eq[String] {
+    def equiv(lft: String, rgt: String):Boolean = lft == rgt
+  }
+  implicit def doubleEq[Double]: Eq[Double] = new Eq[Double] {
+    def equiv(lft: Double, rgt: Double):Boolean = {
+      lft == rgt
+    }
+
+  }
+  implicit def complexEq[Complex]: Eq[Complex] = new Eq[Complex] {
+    def equiv(lft: Complex, rgt: Complex):Boolean = lft == rgt
+  }
     implicit def mapEq[A: Eq]: Eq[Map[A,A]] = new Eq[Map[A,A]]{
       def equiv(lft: Map[A,A], rgt: Map[A,A]): Boolean = {
         val eqA = implicitly[Eq[A]]
-        lft == rgt
+        var  out = true
+        if (lft.size != rgt.size) {
+          return false
+        }
+        for ((k,v) <- lft){
+          out = out && eqA.equiv(lft(k), rgt(k))
+        }
+        out
       }
     }
     implicit def seqEq[A: Eq]: Eq[Seq[A]] = new Eq[Seq[A]]{
       def equiv(lft: Seq[A], rgt: Seq[A]): Boolean = {
         val eqA = implicitly[Eq[A]]
-        lft == rgt
+        var out = true
+        if (lft.size != rgt.size) {
+          return false
+        }
+        for (i <- lft.indices) {
+          out = out && eqA.equiv(lft(i), rgt(i))
+        }
+        out
         }
       }
     implicit def optionEq[A: Eq]: Eq[Option[A]] = new Eq[Option[A]]{
@@ -31,4 +72,7 @@ object Eq {
         lft == rgt
       }
     }
+  implicit class EqOps[T: Eq](val self: T) {
+    def ===(other: T): Boolean = implicitly[Eq[T]].equiv(self, other)
+  }
 }
--- src/test/scala/fintech/homework04/EqSpec.scala
+++ src/test/scala/fintech/homework04/EqSpec.scala
@@ -1,11 +1,17 @@
 package fintech.homework04
 import org.scalatest.{FlatSpec, Matchers}
+import fintech.homework06.Complex
 
 class EqSpec extends FlatSpec with Matchers {
   it should "compare maps correctly 1" in {
     val m1 = Map("a" -> 2)
     val m2 = Map("a" -> 2)
     m1 === m2 shouldEqual true
+  }
+  it should "compare str orrectly 1" in {
+    val s1 = "abc"
+    val s2 = "abc"
+    s1 === s2 shouldEqual true
   }
   it should "compare maps correctly 2" in {
     val m1 = Map("a"->2)
@@ -36,5 +42,10 @@ class EqSpec extends FlatSpec with Matchers {
     val o1 = Option(1)
     val o2 = Option(2)
     o1 === o2 shouldEqual false
+  }
+  it should "compare Complex orrectly 1" in {
+    val c1 = Complex(5.0, 0, 0)
+    val c2 = Complex(5, 0, 0)
+    c1 === c2 shouldEqual true
   }
 }
